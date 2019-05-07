0f8786f657586de0550d162f7770f1c85d8999be
Adeel Hassan <adeel_hassan93@hotmail.com>
2019-05-06 20:59:47 -0400

    handle maps of any size

--- BlockMap.py
+++ BlockMap.py
@@ -8,13 +8,22 @@ from PriorityQueue import PriorityQueue
 
 class BlockMap(object):
 	def __init__(self, Map, block_size):
+		"""Holds the grid of blocks representing the map
+
+		Args:
+			Map: 2D numpy array containing zeros and ones
+			block_size: Map will be divided into block_size x block_size blocks
+		"""
 		super(BlockMap, self).__init__()
+
+		self.block_size = block_size
+		# expand map if not divisible by block size
+		Map = self._expand_map(Map)
+		self._map = Map
 
 		h, w = Map.shape[0] // block_size, Map.shape[1] // block_size
 		self.h, self.w = h, w
-		self.block_size = block_size
 		self.blocks = [[None] * w for _ in range(h)]
-		self._map = Map
 
 		for i in range(h):
 			for j in range(w):
@@ -29,6 +38,19 @@ class BlockMap(object):
 	def __setitem__(self, key, value):
 		y, x = key
 		self.blocks[y][x] = value
+
+	def _expand_map(self, Map):
+		"""Pad map with ones to make its h and w so that it is 
+		evenly divisible into blocks.
+		"""
+		map_h, map_w = Map.shape
+		rem_h, rem_w = map_h % self.block_size, map_w % self.block_size
+		if rem_h > 0:
+			Map = np.concatenate((Map, np.ones((rem_h, map_w))), axis=0)
+		if rem_w > 0:
+			Map = np.concatenate((Map, np.ones((map_h + rem_h, rem_w))), axis=1)
+		return Map
+
 
 	def block_neighbors(self, block):
 		adjacent_blocks = self.adjacent_blocks(block.map_addr)
