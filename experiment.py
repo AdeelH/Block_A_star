b730f820c4cdd8a751ef5492212993c55b4d588b
Adeel Hassan <adeel_hassan93@hotmail.com>
2019-05-07 14:18:45 -0400

    do multiple runs per map

--- experiment.py
+++ experiment.py
@@ -10,26 +10,40 @@ from A_star import a_star
 from common import AttrDict, generate_random_map, l1_dist
 
 
-def experiment(algo, generate_map, data=AttrDict(), preprocessing=lambda m, d: m, runs=100, ignore_failures=True):
+def experiment(algo, generate_map, data=AttrDict(), preprocessing=lambda m, d: m, maps=500, runs=100, ignore_failures=False):
 
 	times = []
 	failure_count = 0
-	while len(times) != runs:
+	for i in range(maps):
+		if i % 10 == 0:
+			print(i)
+
 		_m = generate_map()
-		Map, start, goal = preprocessing(_m, data)
+		m = _m.copy()
 
-		t1 = perf_counter()
-		goal_found, path = algo(Map, start, goal, data)
-		if not goal_found:
-			failure_count += 1
-			if ignore_failures:
-				continue
-		times.append(perf_counter() - t1)
+		h, w = _m.shape
 
-	return sum(times) / runs, times, failure_count
+		starts = [(np.random.randint(h), np.random.randint(w)) for _ in range(runs)]
+		goals = [(np.random.randint(h), np.random.randint(w)) for _ in range(runs)]
+
+		for j, (start, goal) in enumerate(zip(starts, goals)):
+			m = m | _m
+			m[start[0], start[1]] = 0
+			m[goal[0], goal[1]] = 0
+			Map = preprocessing(m, data)
+
+			t1 = perf_counter()
+			goal_found, path = algo(Map, start, goal, data)
+			times.append(perf_counter() - t1)
+			if not goal_found:
+				failure_count += 1
+				if ignore_failures:
+					continue
+
+	return sum(times) / (maps * runs), times, failure_count
 
 
-def time_block_a_star(b_sz, h, w, p, runs):
+def time_block_a_star(b_sz, h, w, p, maps, runs):
 
 	lddb, pathsdb = make_lddb(b_sz, from_file=True, save_to_file=True)
 
@@ -39,31 +53,39 @@ def time_block_a_star(b_sz, h, w, p, runs):
 		'pathsdb': pathsdb,
 		'h': l1_dist
 	})
-	generate_map = lambda: generate_random_map(h, w, p=p)
-	preprocessing = lambda m, d: (BlockMap(m[0], d.block_size), m[1], m[2])
+	generate_map = lambda: generate_random_map(h, w, p=p, start_and_goal=False)
+	preprocessing = lambda m, d: BlockMap(m, d.block_size)
 	algo = lambda m, s, g, d: block_a_star(d.lddb, d.pathsdb, m, s, g, d.h)
 
-	avg_time, _, fc = experiment(algo, generate_map=generate_map, data=data, preprocessing=preprocessing, runs=runs)
+	avg_time, times, fc = experiment(algo, generate_map=generate_map, data=data, preprocessing=preprocessing, maps=maps, runs=runs)
 
 	print('%e' % avg_time, fc)
 
 
-def time_a_star(h, w, p, runs):
+def time_a_star(h, w, p, maps, runs):
 
 	data = AttrDict({
 		'h': l1_dist
 	})
-	generate_map = lambda: generate_random_map(h, w, p=p)
+	generate_map = lambda: generate_random_map(h, w, p=p, start_and_goal=False)
 	algo = lambda m, s, g, d: a_star(m, s, g, d.h)
 
-	avg_time, _, fc = experiment(algo, generate_map=generate_map, data=data, runs=runs)
+	avg_time, times, fc = experiment(algo, generate_map=generate_map, data=data, maps=maps, runs=runs)
 
 	print('%e' % avg_time, fc)
 
 
 np.random.seed(1)
-h, w = 8, 8
-p = 0.4
-runs = 1
-time_block_a_star(b_sz=4, h=h, w=w, p=p, runs=runs)
-time_a_star(h=h, w=w, p=p, runs=runs)
+h, w = 200, 200
+maps = 50
+runs = 100
+
+for p in [0., .1, .2, .3, .4, .5]:
+	print(p)
+	np.random.seed(10)
+	time_a_star(h=h, w=w, p=p, maps=maps, runs=runs)
+
+for p in [0., .1, .2, .3, .4, .5]:
+	print(p)
+	np.random.seed(10)
+	time_block_a_star(b_sz=4, h=h, w=w, p=p, maps=maps, runs=runs)
